a7bbd26f547d9c1b39f2ba27be01e0ae9355ba9c
TomonariUeno <64944610+TomonariUeno@users.noreply.github.com>
2020-11-15 11:32:39 +0900

    First commit

---
+++ nuxt.config.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'microcms-nuxt-jamstack-bolg',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  generate: {
+    async routes() {
+      const pages = await axios
+        .get('https://microcms-nuxt-jamstack-bolg.microcms.io/api/v1/blog?limit=100', {
+          headers: { 'X-API-KEY': 'c2c2749b-e356-438f-882f-1bd87fda1943' }
+        })
+        .then((res) =>
+          res.data.contents.map((content) => ({
+            route: `/${content.id}`,
+            payload: content
+          }))
+        )
+      return pages
+    }
+  },
+
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
